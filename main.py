1845f4010086ffad3342d3a4619f1df5b25f34f5
Alistair <alistairwong@outlook.com>
2017-07-23 10:59:03 -0700

    fixed parser

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 
+import sys
 from bs4 import BeautifulSoup
 
 with open('html_string.txt') as f:
@@ -11,12 +12,20 @@ data = []
 table = soup.find('table')
 
 # table_body = table.find('tbody')
-# TODO fix parser to fit html
+table_body = table
 
-rows = table.find_all('tr')
+# header
+rows = table_body.find('tr') # grab first tr for header
+data.append([ row.text for row in rows ]) # setup fieleds
+
+# data body
+rows = table.find_all('tr')[1:] # skip one tr
 for row in rows:
-    cols = row.find_all('td')
+    cols = row.find_all('td') # not sure why i have to do this inner looping
     cols = [ele.text.strip() for ele in cols]
-    data.append([ele for elel in cols if ele])
+    data.append([ele for ele in cols if ele])
 
+# returns nested lists
 print(data)
+
+# TODO output to csv tsv textfile whatever..
